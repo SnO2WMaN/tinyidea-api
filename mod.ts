39d83279c1af887d6dd7d7b6afe47c6105cfee41
SnO2WMaN <me@sno2wman.net>
2022-11-06 15:50:53 +0900

    `POST /pop` implements wip

--- mod.ts
+++ mod.ts
@@ -1,22 +1,63 @@
-import { Application, Router } from "oak/mod.ts";
 import { MongoClient } from "mongo/mod.ts";
+import { Application, Router } from "oak/mod.ts";
+import { z } from "zod/mod.ts";
 
 const mc = new MongoClient();
 await mc.connect(
   // parsedArgs.mongoUri,
   "mongodb://user:pass@127.0.0.1:27017/tinyidea?authSource=admin",
 );
+const db = mc.database();
 
 const app = new Application();
 const router = new Router();
 
-// const ideasColl = db.collection("ideas");
+const ideasColl = db.collection("ideas");
 
-router.post("/pop", async ({ params, request, response }) => {
-  if (request.hasBody()) {
+const PopPayload = z.object({
+  message: z.string({
+    required_error: "Message is required",
+  }),
+});
+router.post("/pop", async ({ request, response }) => {
+  if (!request.hasBody) {
     response.status = 400;
     return;
   }
+
+  const rawPayload = await request.body({ type: "json" }).value
+    .then((v) => v)
+    .catch(() => undefined);
+  if (rawPayload === undefined) {
+    response.status = 400;
+    response.body = { error: ["Payload cannot parse as JSON"] };
+    return;
+  }
+
+  const parsePayloadResult = PopPayload.safeParse(rawPayload);
+  if (!parsePayloadResult.success) {
+    response.status = 400;
+    response.body = { error: parsePayloadResult.error.format().message?._errors };
+    return;
+  }
+
+  const { message } = parsePayloadResult.data;
+
+  const unique = `${Date.now()}`;
+  const storeIdea = await ideasColl
+    .insertOne({
+      unique,
+      message,
+      created_at: new Date(),
+    })
+    .catch(() => undefined);
+  if (!storeIdea) {
+    response.status = 500;
+    response.body = { error: ["Something broken when storing data"] };
+    return;
+  }
+  response.body = { unique };
+  return;
 });
 
 app.use(router.routes());
